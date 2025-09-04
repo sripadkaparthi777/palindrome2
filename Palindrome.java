7e2501fc3b8a90c5af1b53c907150f1aaadab618
saiteja-053 <saitejakatkam53@gmail.com>
2025-09-04 11:33:20 +0530

    add palindrome program with cli argument

--- Palindrome.java
+++ Palindrome.java
@@ -6,13 +6,13 @@ public class Palindrome {
         }
         String word = args[0];
         if (isPalindrome(word)) {
-            System.out.println(word + " is a palindrome!");
+            System.out.printlnx(word + " is a palindrome!");
         } else {
             System.out.println(word + " is NOT a palindrome!");
         }
     }
     public static boolean isPalindrome(String str) {
-        String reversed = new StringBuilder(str).reverse().toString();
+        String reversed = nexw StringBuilder(str).reverse().toString();
         return str.equalsIgnoreCase(reversed);
     }
 }
